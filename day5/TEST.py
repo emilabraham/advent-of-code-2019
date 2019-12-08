f2b47b9abd52a2deea86676d5a364e9cf3e20147
Emil Abraham <emilabraham1414@gmail.com>
2019-12-08 00:07:22 -0500

    Day 5 complete

--- day5/TEST.py
+++ day5/TEST.py
@@ -174,6 +174,18 @@ instructions = [
 testInstructions = [1002,4,3,4,33]
 testInstructions2 = [3,0,4,0,99]
 
+testInstructionsEqual8 = [3,9,8,9,10,9,4,9,99,-1,8]
+testInstructionsLess8 = [3,9,7,9,10,9,4,9,99,-1,8]
+testInstructionsEqual8Param = [3,3,1108,-1,8,3,4,3,99]
+testInstructionsLess8Param = [3,3,1107,-1,8,3,4,3,99]
+otherInst = [1,0,3,3,1005,2,10,5,1,0,4,1,99]
+countdown = [101,-1,7,7,4,7,1105,11,0,99]
+largerExample = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+                 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+                 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+zero = [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]
+zero2 = [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]
+
 # Prompt for input and store at the next position's value. Return new head
 # position
 def inputoc(head, program):
@@ -186,6 +198,37 @@ def output(head, program):
     print(program[program[head+1]])
     return head+2
 
+# If parameter is not zero, jump to second parameter. Otherwise, continue.
+def jumpTrue(head, program):
+    if program[head+1] != 0:
+        return program[head+2]
+    else:
+        return head+3
+
+# If parameter is zero, jump to second parameter. Otherweise, continue.
+def jumpFalse(head, program):
+    if program[head+1] == 0:
+        return program[head+2]
+    else:
+        return head+3
+
+# If parameter 1 < parameter 2, write 1 to parameter 3. Otherwise write 0
+def lessThan(head, program):
+    if program[program[head+1]] < program[program[head+2]]:
+        program[program[head+3]] = 1
+    else:
+        program[program[head+3]] = 0
+    return head+4
+
+# If parameter 1 == paremeter 2, write 1 to paremeter 3. Otherwise write 0
+def equals(head, program):
+    if program[program[head+1]] == program[program[head+2]]:
+        program[program[head+3]] = 1
+    else:
+        program[program[head+3]] = 0
+    return head+4
+
+
 def run(myinstructions):
     program = copy.copy(myinstructions)
     head = 0
@@ -193,7 +236,7 @@ def run(myinstructions):
     while not halt:
         if program[head] == 99: #halt
             print("Found a halt")
-            print(program)
+            #print(program)
             halt = True
         elif program[head] == 1: #add
             #print("Current operation: %d, %d, %d, %d" % (program[head],
@@ -216,13 +259,19 @@ def run(myinstructions):
             #print("Current operation: %d, %d" % (program[head],
             #                                     program[head+1]))
             head = output(head, program)
+        elif program[head] == 5: # jump-if-true
+            head = jumpTrue(head, program)
+        elif program[head] == 6: # jump-if-false
+            head = jumpFalse(head, program)
+        elif program[head] == 7: # less than
+            head = lessThan(head, program)
+        elif program[head] == 8: # equals
+            head = equals(head, program)
         else:
             head = parameterMode(head, program)    
 
     return program[0]
 
-# TODO: Might need to handle 3. Not sure yet. Just make sure it isn't completely
-# broken for now
 def parameterMode(head, program):
     param = program[head]
     opcode = program[head] % 10
@@ -250,9 +299,28 @@ def parameterMode(head, program):
         #print("Current operation: %d, %d" % (program[head],
         #                                     value))
         return outputParam(head, value)
+    elif opcode == 5:
+        firstValue = checkMode(program[head+1], opInstructions[0], program)
+        secondValue = checkMode(program[head+2], opInstructions[1], program)
+        return jumpTrueParam(head, firstValue, secondValue, program)
+    elif opcode == 6:
+        firstValue = checkMode(program[head+1], opInstructions[0], program)
+        secondValue = checkMode(program[head+2], opInstructions[1], program)
+        return jumpFalseParam(head, firstValue, secondValue, program)
+    elif opcode == 7:
+        firstValue = checkMode(program[head+1], opInstructions[0], program)
+        secondValue = checkMode(program[head+2], opInstructions[1], program)
+        destination = program[head+3]
+        return lessThanParam(head, firstValue, secondValue, destination,
+                             program)
+    elif opcode == 8:
+        firstValue = checkMode(program[head+1], opInstructions[0], program)
+        secondValue = checkMode(program[head+2], opInstructions[1], program)
+        destination = program[head+3]
+        return equalsParam(head, firstValue, secondValue, destination,
+                           program)
     else:
         print("Something went wrong inside parameterMode")
-        #print("Head: % 3d, Value: % 3d" % (head, program[head]))
         halt = True
 
 # Based on the opcode return either the immediate or position
@@ -260,14 +328,47 @@ def parameterMode(head, program):
 def checkMode(param, code, program):
     return param if code == 1 else program[param]
 
+# The parameter version of add
 def addParam(head, firstVal, secondVal, destination, program):
     program[destination] = firstVal + secondVal
     return head+4
 
+# The parameter version of multiply
 def multiplyParam(head, firstVal, secondVal, destination, program):
     program[destination] = firstVal * secondVal
     return head+4
 
+# The parameter version of jumpTrue
+def jumpTrueParam(head, firstVal, secondVal, program):
+    if firstVal != 0:
+        return secondVal
+    else:
+        return head+3
+
+# The parameter version of jumpFalse
+def jumpFalseParam(head, firstVal, secondVal, program):
+    if firstVal == 0:
+        return secondVal
+    else:
+        return head+3
+
+# The parameter version of lessThan
+def lessThanParam(head, firstVal, secondVal, destination, program):
+    if firstVal < secondVal:
+        program[destination] = 1
+    else:
+        program[destination] = 0
+    return head+4
+
+# The parameter of equals
+def equalsParam(head, firstVal, secondVal, destination, program):
+    if firstVal == secondVal:
+        program[destination] = 1
+    else:
+        program[destination] = 0
+    return head+4
+
+# The parameter version of output
 def outputParam(head, value):
     print(value)
     return head+2
@@ -295,4 +396,12 @@ def digitPlace(digit, position):
 
 #print(run(testInstructions))
 #print(run(testInstructions2))
+#print(run(testInstructionsEqual8))
+#print(run(testInstructionsLess8))
+#print(run(testInstructionsEqual8Param))
+#print(run(testInstructionsLess8Param))
+#print(run(otherInst))
+#print(run(countdown))
+#print(run(largerExample))
+#print(run(zero))
 print(run(instructions))
